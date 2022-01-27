16f1db6818afde8ae0c90bf94e1f70f581e13e61
Mads Jenkins <madelinehjenkins@gmail.com>
2022-01-27 10:03:58 +0000

    switch to plus

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -23,7 +23,7 @@ public class QueryProcessor {
         }
 
         if (query.toLowerCase().contains("what is")) {
-            string[] s= query.split(":")[1].split(",");
+            string[] s= query.split(":")[1].split(" plus ");
             return Integer.parseInt(s[0]) + Integer.parseInt(s[1]);
         }
 
